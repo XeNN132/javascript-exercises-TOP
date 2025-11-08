b3bd6accf8c565ed87d61b53a7be4a3a4995df3c
David Adesina <davidereadesina@gmail.com>
2025-11-08 23:42:41 +0100

    Practiced functions js

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+	if(times < 0) return "ERROR";
+	var string = "";
+	for (i = 0; i < times; i++){
+		string+=str;
+	}
+	return string;
 };
 
 // Do not edit below this line
