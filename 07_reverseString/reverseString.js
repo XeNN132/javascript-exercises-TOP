7e9a15b5203006be595c986b4545aec03becb52a
David Adesina <davidereadesina@gmail.com>
2025-11-11 12:52:06 +0100

    Wrote a function to reverse a string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function( inputString ) {
+	const lengthOfInputString = inputString.length;
+	var newString = Array.from(inputString) ;
+	for (var i = 1; i <= lengthOfInputString; i++){
+		newString[i - 1] = inputString[lengthOfInputString - i];
+	}
+	return newString.join('');
 };
 
 // Do not edit below this line
